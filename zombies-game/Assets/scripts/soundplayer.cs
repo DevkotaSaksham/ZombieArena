53e0a286ba945169f9e0c0bdb3ba13cfd3e16820
cyrusmew <51240567+Cyrusroy001@users.noreply.github.com>
2021-09-26 10:37:10 +0545

    idk some changes

--- zombies-game/Assets/scripts/soundplayer.cs
+++ zombies-game/Assets/scripts/soundplayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class soundplayer : MonoBehaviour
 {
     public AudioSource bgmusic;
+    // public static bool musicToggle = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +13,22 @@ public class soundplayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    // void Update()
+    // {
+    //     if(!musicToggle){
+    //         bgmusic.Pause();
+    //     }else{
+    //         bgmusic.Play();
+    //     }
         
-    }
+    // }
+
+    // public void isPressed ()
+    // {
+    //     if(musicToggle){
+    //         musicToggle = false;
+    //     }else{
+    //         musicToggle = true;
+    //     }
+    // }
 }
